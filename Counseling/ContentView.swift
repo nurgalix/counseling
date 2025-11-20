2c3b00e33c8f9c07a5b18acb71688ce3792ee6a0
Nurgali Nuraly <nurali.nurgali@gmail.com>
2025-11-20 19:22:17 +0500

    Initial Commit

---
+++ Counseling/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Counseling
+//
+//  Created by Nurgali on 20.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Counseling/CounselingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounselingApp.swift
+//  Counseling
+//
+//  Created by Nurgali on 20.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct CounselingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
